d07b1c62ed23882c83eb0e245ccc1f9b86fa21d7
hch <kejun.hch@qq.com>
2021-09-05 13:28:16 +0800

    fix: springboot bench log

---
+++ springboot-bench/src/main/java/cc/kejun/conf/WebConfig.java
@@ -0,0 +1,37 @@
+package cc.kejun.conf;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author hch
+ * @since 2021/9/5
+ */
+@Slf4j(topic = "monitor")
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HandlerInterceptor() {
+            private static final String START = "start";
+
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                request.setAttribute(START, System.currentTimeMillis());
+                return true;
+            }
+
+            @Override
+            public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+                long start = (long) request.getAttribute(START);
+                log.info("{} costs {}ms", request.getServletPath(), System.currentTimeMillis() - start);
+            }
+        });
+    }
+}
