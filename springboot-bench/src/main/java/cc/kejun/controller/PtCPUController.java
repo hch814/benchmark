b6019f418655f4d7ca30ad71283dd3d4b5fed9ac
hch <kejun.hch@qq.com>
2021-09-11 09:39:15 +0800

    feat: cpu

--- springboot-bench/src/main/java/cc/kejun/controller/PtCPUController.java
+++ springboot-bench/src/main/java/cc/kejun/controller/PtCPUController.java
@@ -3,16 +3,19 @@ package cc.kejun.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 /**
  * @author hch
  * @since 2021/9/11
  */
 @RestController
 public class PtCPUController {
+    private static final Random RANDOM = new Random();
     @GetMapping("/cpu")
     public void cpu() {
         for (int i = 0; i < 10000; i++) {
-            Math.log(1.1);
+            Math.log(RANDOM.nextDouble());
         }
     }
 }
